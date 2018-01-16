6895ac5bc268fc8013293912d32213b6636f1529
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2018-01-16 11:47:50 +0100

    scaffold

--- lib/rys/rails_generator.rb
+++ lib/rys/rails_generator.rb
@@ -21,3 +21,27 @@ module Rys
     end
   end
 end
+
+module Rys
+  module RailsGenerator
+    class Scaffold < ::Rails::Generators::NamedBase
+
+      def self.help(shell)
+        shell.say "Usage for Rys:"
+        shell.say "  rails generate rys:scaffold RYS_PLUGIN NAME ...same as below..."
+        shell.say
+        Rails::Generators.find_by_namespace('scaffold').help(shell)
+      end
+
+      def self.namespace
+        "rys:scaffold"
+      end
+
+      def invoke_origin
+        plugin = ARGV.shift
+        Rails::Generators.invoke 'scaffold', ARGV, behavior: :invoke, destination_root: "rys_plugins/#{plugin}"
+      end
+
+    end
+  end
+end
