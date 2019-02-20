69e3915163be43891e4de98f5297c57e320dc15c
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2019-02-20 13:06:40 +0100

    refs #276755 singletone class

--- lib/rys/plugins_management.rb
+++ lib/rys/plugins_management.rb
@@ -1,18 +1,23 @@
-require 'singleton'
-
 module Rys
   class PluginsManagement
-    include Singleton
-    attr_reader :plugins
+
+    mattr_accessor :all_plugins
+    self.all_plugins = []
+
+    # Backward compatibility
+    def self.instance
+      ActiveSupport::Deprecation.warn('.instance method is deprecated')
+      self
+    end
 
     def self.add(engine_klass)
-      instance.add(engine_klass)
+      all_plugins << engine_klass
     end
 
     # @param delegate_with [Delegator] Wrap plugin with this delegator
     # @param systemic [true, false] Returns also systemic ryses
     def self.all(delegate_with: nil, systemic: false)
-      plugins = instance.plugins
+      plugins = all_plugins.dup
 
       if !systemic
         plugins.select! {|p| !p.parent.config.systemic }
@@ -34,18 +39,10 @@ module Rys
     def self.find(plugin)
       case plugin
       when String, Symbol
-        instance.plugins.find{|p| p.rys_id == plugin.to_s }
+        all_plugins.find{|p| p.rys_id == plugin.to_s }
       when ::Rails::Engine
-        instance.plugins.find{|p| p == plugin }
+        all_plugins.find{|p| p == plugin }
       end
-    end
-
-    def initialize
-      @plugins = []
-    end
-
-    def add(engine_klass)
-      @plugins << engine_klass
     end
 
   end
